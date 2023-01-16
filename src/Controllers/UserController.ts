869445fa6f2961873069ee2909223d0dde849877
Luipee <lipeffcampos@gmail.com>
2023-01-16 16:14:01 -0300

    Controllers e routes

---
+++ src/Controllers/UserController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express"
+
+
+const users =[
+  {name: 'Diego ' , email:'diego@gmail.com'}
+]
+
+
+export default{}
---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import UserController from "./Controllers/UserController";
+
+
+const routes = Router()
+
+routes.get('/users', UserController.index)
+export default routes
